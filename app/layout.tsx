9c16a9c621efed8c5b1558f9510fd8736848fee3
JacquesCastel <49317389+JacquesCastel@users.noreply.github.com>
2025-11-04 11:56:27 +0100

    fix: CTA WE

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import HeaderWrapper from './components/HeaderWrapper';
 export const dynamic = 'force-dynamic';
 export const metadata: Metadata = {
   title: "Wharf - Design Narratif",
-  description: "Agence de communication corporate et design narratif",
+  description: "Agence de design narratif et de production audiovisuelle",
 };
 
 export default function RootLayout({
